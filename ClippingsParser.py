02b4526140a8fbf654e42ac2e8ccd93bd44727df
dmserg <dmserg@gmail.com>
2017-07-10 01:30:40 +0300

    Bugfix
    Fix bug of bookmarks processing
    Minor refactorings and clean up

--- ClippingsParser.py
+++ ClippingsParser.py
@@ -1,6 +1,10 @@
-def mapper( mr, record):
+import sys
+
+
+def mapper(mr, record):
     if record['type'] == 'Highlight':
         mr.emit_intermediate(record['title'], record['body'])
+
 
 def reducer(mr, key, records):
     record = dict()
@@ -8,50 +12,53 @@ def reducer(mr, key, records):
     record['Title'] = key
     mr.emit(record)
 
-class ClippingsParser:
-    from pprint import pprint
 
+class ClippingsParser:
     def __init__(self):
         self.EOR = u"=========="
 
     def clippingRecords(self, file_path):
         import re
         import codecs
-        clip_file = codecs.open( file_path )
-        clip_file.seek( 3 ) # skip magic cookie
+        clip_file = codecs.open(file_path)
+        clip_file.seek(3)  # skip magic cookie
 
         record = list()
         for line in clip_file:
-            line = line.decode( 'utf-8' )
+            line = line.decode('utf-8')
             if line.strip() == self.EOR:
-                assert record[2] == '', "Non-blank line expected separating the header from the body of the clipping:%s" % record[2]
+                assert record[2] == '', u"Non-blank line expected separating the header from the body of the clipping:{0:s}" \
+                    .format(record[2])
                 clip = dict()
-                #match = re.match( r'(.*?)\((.*)\)', record[0] )
-                match = re.match( r'(.*?)\(([^(]*)\)$', record[0] )
+                match = re.match(r'(.*?)\(([^(]*)\)$', record[0])
                 if match:
-                    clip['title'], clip['attribution'] =match.groups() 
-                    clip['attribution'] = clip['attribution'].split( ') (' )
+                    clip['title'], clip['attribution'] = match.groups()
+                    clip['attribution'] = clip['attribution'].split(') (')
                 else:
                     # pattern isn't matched
                     clip['title'] = record[0]
                     clip['attribution'] = ''
 
-                match = re.match( r'- (\w+) Loc. ([^|]+)\| Added on (\w+), (\w+ \d+, \d+), (\d+:\d+ \w\w)', record[1] )
-                clip['type'], clip['location'], clip['dow'], clip['date'], clip['time'] = match.groups()
+                try:
+                    match = re.match(r'- (.+) Loc. ([^|]+)\| Added on (\w+), (\w+ \d+, \d+), (\d+:\d+ \w\w)',
+                                     record[1])
 
-                clip['body'] = "\n".join( record[3:] )
+                    clip['type'], clip['location'], clip['dow'], clip['date'], clip['time'] = match.groups()
 
-                # a little tidying
-                clip['title'] = clip['title'].strip()
-                clip['location'] = clip['location'].strip()
-                # yield and reset for next record
-                yield clip
-                record = list() 
+                    clip['body'] = "\n".join(record[3:])
+
+                    # a little tidying
+                    clip['title'] = clip['title'].strip()
+                    clip['location'] = clip['location'].strip()
+                    # yield and reset for next record
+                    yield clip
+                    record = list()
+                except:
+                    print("Unexpected error:", sys.exc_info()[0])
             else:
-                record.append( line.strip() )
+                record.append(line.strip())
 
         clip_file.close()
-
 
     def parseClippings(self, fileName):
         from MapReduce import MapReduce
